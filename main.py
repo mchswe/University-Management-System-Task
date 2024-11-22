8dcc13a91203d61e6ae57b2e3957c41bf7cce539
mchswe <michael.sewell@laetottenham.ac.uk>
2024-11-22 12:25:51 +0000

    avg grade function broken

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Student(Person):
         super().__init__(given_name, given_age, given_gender)
         self.student_id = ""
         self.course = ""
-        self.grades = []
+        self.grades = [0]
 
     def set_student_details(self, student_id, course):
         self.student_id = student_id
@@ -27,47 +27,75 @@ class Student(Person):
         self.grades = self.grades.append(grade)
     
     def calculate_average_grade(self):
-        len = len(self.grades)
-        sum = sum(self.grades)
-        avg_grade = len / sum
-        return avg_grade
+        for x in int(str(self.grades)):
+            summ = sum(x)
+            return summ
+        count = 0
+        for z in self.grades:
+            count += 1
+            z = count
+        avg = x / z
+        return avg
     
     def get_student_summary(self):
         print(f"Name: {self.name}, Student ID: {self.student_id}, Course: {self.course}, Grades: {self.grades} ")
     
-    class Professor(Person):
-        def __init__(self,given_name, given_age, given_gender):
-            super().__init__(given_name, given_age, given_gender)
-            self.staff_id = ""
-            self.department = ""
-            self.salary = ""
+class Professor(Person):
+    def __init__(self,given_name, given_age, given_gender):
+        super().__init__(given_name, given_age, given_gender)
+        self.staff_id = ""
+        self.department = ""
+        self.salary = ""
+    
+    def set_professor_details(self, staff_id, department, salary):
+        self.staff_id = staff_id
+        self.department = department
+        self.salary = salary
+    
+    def give_feedback(self, student, feedback):
+        print(f"Feedback for {student.name}: {feedback}")
+    
+    def increase_salary(self, percentage):
+        multiplier = percentage / 100
+        new_salary = self.salary * multiplier
+        self.salary = new_salary
+    
+    def get_professor_summary(self):
+        print(f"Name: {self.name}, Staff ID: {self.staff_id}, Department: {self.department}, Salary: {self.salary}")
+    
+class Administrator(Person):
+    def __init__(self, given_name, given_age, given_gender):
+        super().__init__( given_name, given_age, given_gender)
+        self.admin_id = ""
+        self.office = ""
+        self.years_of_service = 0
+    
+    def set_admin_details(self, admin_id, office, years_of_service):
+        self.admin_id = admin_id
+        self.office = office
+        self.years_of_service = years_of_service
+    
+    def increment_service_years(self):
+        self.years_of_service += 1
+
+    def get_admin_summary(self):
+        print(f"Name: {self.name}, Admin ID: {self.admin_id}, Office: {self.office}, Years Of Service: {self.years_of_service}")
         
-        def set_professor_details(self, staff_id, department, salary):
-            self.staff_id = staff_id
-            self.department = department
-            self.salary = salary
-        
-        def give_feedback(self, student, feedback):
-            print(f"Feedback for {student.name}: {feedback}")
-        
-        def increase_salary(self, percentage):
-            multiplier = percentage / 100
-            new_salary = self.salary * multiplier
-            self.salary = new_salary
-        
-        def get_professor_summary(self):
-            print(f"Name: {self.name}, Staff ID: {self.staff_id}, Department: {self.department}, Salary: {self.salary}")
-        
-        class Administrator(Person):
-            def __init__(self, given_name, given_age, given_gender):
-                super().__init__(self, given_name, given_age, given_gender)
-                self.admin_id = ""
-                self.office = ""
-                self.years_of_service = ""
-            
-            def set_admin_details(self, admin_id, office, years_of_service):
-                self.admin_id = admin_id
-                self.office = office
-                self.years_of_service = years_of_service
-            
-            def 
+may = Student("May", 19, "Female")
+ben = Student("Ben", 21, "Male")
+otto = Professor("Otto", 39, "Male")
+osborn = Professor("Osborn", 44, "Male")
+thompson = Administrator("Thompson", 41, "Male")
+
+may.add_grade(9)
+# may.calculate_average_grade()
+otto.give_feedback(may, "Well Done on getting an A in your Midterm.")
+otto.set_professor_details("ABD333", "Physics", 1500)
+otto.increase_salary(4)
+thompson.increment_service_years()
+
+may.get_student_summary()
+ben.get_student_summary()
+otto.get_professor_summary()
+osborn.get_professor_summary()
+thompson.get_admin_summary()
