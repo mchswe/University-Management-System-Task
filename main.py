c8b8280834cfef41c08501b8a1d8f858f3e320de
mchswe <michael.sewell@laetottenham.ac.uk>
2024-11-22 11:57:17 +0000

    prof

--- main.py
+++ main.py
@@ -38,4 +38,36 @@ class Student(Person):
     class Professor(Person):
         def __init__(self,given_name, given_age, given_gender):
             super().__init__(given_name, given_age, given_gender)
+            self.staff_id = ""
+            self.department = ""
+            self.salary = ""
+        
+        def set_professor_details(self, staff_id, department, salary):
+            self.staff_id = staff_id
+            self.department = department
+            self.salary = salary
+        
+        def give_feedback(self, student, feedback):
+            print(f"Feedback for {student.name}: {feedback}")
+        
+        def increase_salary(self, percentage):
+            multiplier = percentage / 100
+            new_salary = self.salary * multiplier
+            self.salary = new_salary
+        
+        def get_professor_summary(self):
+            print(f"Name: {self.name}, Staff ID: {self.staff_id}, Department: {self.department}, Salary: {self.salary}")
+        
+        class Administrator(Person):
+            def __init__(self, given_name, given_age, given_gender):
+                super().__init__(self, given_name, given_age, given_gender)
+                self.admin_id = ""
+                self.office = ""
+                self.years_of_service = ""
             
+            def set_admin_details(self, admin_id, office, years_of_service):
+                self.admin_id = admin_id
+                self.office = office
+                self.years_of_service = years_of_service
+            
+            def 
